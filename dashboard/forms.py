36fbf4af0a30c178c5649084c291e8e0adc11eae
monikashah <monikapatira@gmail.com>
2023-03-13 23:06:11 +0530

    changed model name for uplaod wells

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -4,5 +4,5 @@ from django import forms
 
 class UploadWellPictureForm(forms.ModelForm):
     class Meta:
-        model = upload_well
+        model = 'upload_well'
         fields = ('picture','name','well_nm','radius','depth','level','village','district','state','pincode', 'lat', 'lng')
