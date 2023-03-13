9315f0fd529d22cc1530df5425dbbb473076cbab
monikashah <monikapatira@gmail.com>
2023-03-13 23:15:51 +0530

    model name rectified

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -4,5 +4,5 @@ from django import forms
 
 class UploadWellPictureForm(forms.ModelForm):
     class Meta:
-        model = 'upload_well'
+        model = UploadWellPictureModel
         fields = ('picture','name','well_nm','radius','depth','level','village','district','state','pincode', 'lat', 'lng')
