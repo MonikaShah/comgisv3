749734ce6cfc1792cb6691df67ad611a5f205919
monikashah <monikapatira@gmail.com>
2023-03-13 22:59:46 +0530

    Pillow issue resolved

--- dashboard/models.py
+++ dashboard/models.py
@@ -16,42 +16,42 @@ from django.contrib.gis.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 # from django.contrib.auth.models import User
 from django import forms
-# from PIL import Image
-# from io import BytesIO
-# import sys
+from PIL import Image
+from io import BytesIO
+import sys
 from django.core.files.uploadedfile import InMemoryUploadedFile
 # from django import InMemoryUploadedFile
 from crispy_forms.helper import FormHelper
 
-# class UploadWellPictureModel(models.Model):
-#     picture = models.ImageField( upload_to='WellPics/', blank=True, null=True, default='WellPics/noImage.jpg')
-#     name = models.CharField(max_length=100, blank=True, null=True)
-#     well_nm = models.CharField(max_length=100, blank=True, null=True)
-#     radius = models.IntegerField(blank=True, null=True)
-#     depth = models.IntegerField(blank=True, null=True)
-#     level = models.IntegerField(blank=True, null=True)
-#     village = models.CharField(max_length=100, blank=True, null=True)
-#     district = models.CharField(max_length=100, blank=True, null=True)
-#     state = models.CharField(max_length=100, blank=True, null=True)
-#     pincode = models.CharField(max_length=8, blank=True, null=True)
-#     lat = models.CharField(max_length=15)
-#     lng = models.CharField(max_length=15)
-#     def save(self, *args, **kwargs):
-#         if not self.id:
-#             self.picture = self.compressImage(self.picture)
-#         super(UploadWellPictureModel, self).save(*args, **kwargs)
-#     def compressImage(self,picture):
-#         imageTemproary = Image.open(picture)
-#         imageTemproary = imageTemproary.convert('RGB')
-#         outputIoStream = BytesIO()
-#         imageTemproary = imageTemproary.resize( (1020,573) ) 
-#         imageTemproary.save(outputIoStream , format='JPEG', quality=60)
-#         outputIoStream.seek(0)
-#         picture = InMemoryUploadedFile(outputIoStream,'ImageField', "%s.jpg" % picture.name.split('.')[0], 'image/jpeg', sys.getsizeof(outputIoStream), None)
-#         return picture
-#     class Meta:
-#        managed = True
-#        db_table = 'upload_well'
+class UploadWellPictureModel(models.Model):
+    picture = models.ImageField( upload_to='WellPics/', blank=True, null=True, default='WellPics/noImage.jpg')
+    name = models.CharField(max_length=100, blank=True, null=True)
+    well_nm = models.CharField(max_length=100, blank=True, null=True)
+    radius = models.IntegerField(blank=True, null=True)
+    depth = models.IntegerField(blank=True, null=True)
+    level = models.IntegerField(blank=True, null=True)
+    village = models.CharField(max_length=100, blank=True, null=True)
+    district = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    pincode = models.CharField(max_length=8, blank=True, null=True)
+    lat = models.CharField(max_length=15)
+    lng = models.CharField(max_length=15)
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.picture = self.compressImage(self.picture)
+        super(UploadWellPictureModel, self).save(*args, **kwargs)
+    def compressImage(self,picture):
+        imageTemproary = Image.open(picture)
+        imageTemproary = imageTemproary.convert('RGB')
+        outputIoStream = BytesIO()
+        imageTemproary = imageTemproary.resize( (1020,573) ) 
+        imageTemproary.save(outputIoStream , format='JPEG', quality=60)
+        outputIoStream.seek(0)
+        picture = InMemoryUploadedFile(outputIoStream,'ImageField', "%s.jpg" % picture.name.split('.')[0], 'image/jpeg', sys.getsizeof(outputIoStream), None)
+        return picture
+    class Meta:
+       managed = True
+       db_table = 'upload_well'
 # class CustomAccountManager(BaseUserManager):
 #     def create_superuser(self, username, password, **other_fields):
 #         default_ward = MumbaiWardBoundary2Jan2022.objects.get(id=1)
